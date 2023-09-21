dd30a8553bd20d08ea84dd4d6cc2e815f30d2e8b
ElianArism <elianarism@gmail.com>
2023-09-21 16:37:13 -0300

    implementing quizz view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'poke-quizz';
+export class AppComponent implements OnInit {
+  private router: Router = inject(Router);
+  public bg = 'bg-red';
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe((navigationObj) => {
+        this.bg =
+          (navigationObj as NavigationEnd).url === '/home'
+            ? 'bg-red'
+            : 'bg-white';
+      });
+  }
 }
--- src/app/pages/quizz/quizz.component.ts
+++ src/app/pages/quizz/quizz.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import * as color from 'color.js';
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.scss']
+  styleUrls: ['./quizz.component.scss'],
 })
-export class QuizzComponent {
+export class QuizzComponent implements AfterViewInit {
+  @ViewChild('pokemonImg')
+  private pokemonImg!: ElementRef<HTMLImageElement>;
+  @ViewChild('pokemonCard')
+  private pokemonCard!: ElementRef<HTMLElement>;
 
+  private r2: Renderer2 = inject(Renderer2);
+
+  ngAfterViewInit(): void {
+    color
+      .prominent(this.pokemonImg.nativeElement.src, {
+        format: 'hex',
+        amount: 4,
+      })
+      .then((colors) => {
+        console.log(colors);
+        console.log(
+          `linear-gradient(to top, ${colors[1]}, ${colors[2]}, ${colors[3]})`
+        );
+        this.r2.setStyle(
+          this.pokemonCard.nativeElement,
+          'background',
+          `linear-gradient(to top, ${colors[1]}, ${colors[2]}, ${colors[3]})`
+        );
+      })
+      .catch(console.log);
+  }
 }
