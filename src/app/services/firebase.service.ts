00260812f46fe2444dfaf72e99032c5bd5c222c8
ElianArism <elianarism@gmail.com>
2023-10-25 18:59:27 -0300

    create firestore service and implement main methods to store trainers

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 
 import { FirebaseApp, initializeApp } from 'firebase/app';
-import { Firestore, getFirestore } from 'firebase/firestore';
+import {
+  CollectionReference,
+  DocumentData,
+  collection,
+  doc,
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import { ITrainer } from '../interfaces/trainer.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
   private app!: FirebaseApp;
-  private db!: Firestore;
+  private collection!: CollectionReference<DocumentData, DocumentData>;
 
   constructor() {
     this.connectWithFirestore();
@@ -25,8 +37,55 @@ export class FirebaseService {
       appId: '1:482917161107:web:9961beefadd15c925ad566',
     });
 
-    this.db = getFirestore(this.app);
+    const db = getFirestore(this.app);
+    this.collection = collection(db, 'trainers');
+
     // Continue from here
     // https://firebase.google.com/docs/firestore/query-data/get-data
+  }
+
+  async saveTrainer(trainer: {
+    trainerName: string;
+    imgLogo?: string;
+    password: string;
+    score: number;
+  }) {
+    try {
+      const trainerDoc = await this.getTrainerByUsername(trainer.trainerName);
+      if (trainerDoc.exists()) {
+        return;
+      }
+      await setDoc(doc(this.collection), trainer);
+    } catch (error: any) {
+      throw new Error(error?.message);
+    }
+  }
+
+  async updateTrainer(username: string, trainer: Partial<ITrainer>) {
+    try {
+      const trainerDoc = await this.getTrainerByUsername(username);
+      await updateDoc(trainerDoc.ref, trainer);
+    } catch (error: any) {
+      throw new Error(error?.message);
+    }
+  }
+
+  async getTrainerByUsername(username: string): Promise<any> {
+    try {
+      const trainerQuery = query(
+        this.collection,
+        where('trainerName', '==', username)
+      );
+
+      const queryResults = await getDocs(trainerQuery);
+
+      if (!queryResults.docs[0]?.exists()) {
+        throw new Error('Trainer does not exist');
+      }
+
+      return queryResults.docs[0];
+    } catch (error: any) {
+      throw new Error(error?.message);
+    }
   }
 }
