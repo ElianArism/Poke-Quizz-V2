22ebce3e676a2617ce9da5e961de802bb1f0f43c
Elian Arismendi <naranjax@MAC-68553.local>
2024-02-29 22:11:31 -0300

    feat: finish select input, start formControl and reactive forms implementation

--- src/app/components/login-modal/login-modal.component.ts
+++ src/app/components/login-modal/login-modal.component.ts
@@ -8,10 +8,11 @@ import {
   inject,
   signal,
 } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { Pokemon } from 'src/app/interfaces/pokemon.interface';
+import { FirebaseService } from 'src/app/services/firebase.service';
 import { AppState } from 'src/app/store/app.store';
 import { pokemonsSelector } from 'src/app/store/selectors/pokemon.selector';
 
@@ -25,28 +26,59 @@ import { pokemonsSelector } from 'src/app/store/selectors/pokemon.selector';
 export class LoginModalComponent implements OnInit, OnDestroy {
   private readonly store: Store<AppState> = inject(Store);
   private readonly pokemonSelector = this.store.select(pokemonsSelector);
+  private readonly firebaseService: FirebaseService = inject(FirebaseService);
 
-  private subscription!: Subscription;
+  private subscriptions: Subscription[] = [];
 
   @Input('openModal')
   openModal: boolean = false;
 
   pokemons$: WritableSignal<Pokemon[]> = signal([]);
+  pokemonList$: WritableSignal<Pokemon[]> = signal(this.pokemons$());
+  selectedPokemonControl: FormControl = new FormControl<string>('', {
+    nonNullable: true,
+  });
 
   ngOnInit(): void {
-    this.subscription = this.pokemonSelector.subscribe(({ pokemons }) => {
-      this.pokemons$.set(pokemons);
-      console.log(this.pokemons$());
-    });
+    this.subscriptions.push(
+      this.pokemonSelector.subscribe(({ pokemons }) => {
+        this.pokemons$.set(pokemons);
+      })
+    );
+
+    this.subscriptions.push(
+      this.selectedPokemonControl.valueChanges.subscribe(
+        this.searchPokemon.bind(this)
+      )
+    );
   }
 
   hideLoginModal(): void {
     this.openModal = false;
   }
 
-  async saveTrainer(): Promise<void> {}
+  async saveTrainer(): Promise<void> {
+    // this.firebaseService.saveTrainer({});
+  }
+
+  searchPokemon(value: string): void {
+    if (!value) {
+      this.pokemonList$.set(this.pokemons$());
+      return;
+    }
+    this.pokemonList$.set(
+      this.pokemons$().filter((p) => p.name.includes(value))
+    );
+    if (this.pokemonList$()[0].name === this.selectedPokemonControl.value) {
+      this.pokemonList$.set([]);
+    }
+  }
+
+  setSelectedPokemon(pokemonName: string): void {
+    this.selectedPokemonControl.setValue(pokemonName);
+  }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 }
